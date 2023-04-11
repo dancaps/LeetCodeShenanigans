65587ecbce85ff8f26c4efe8d0568c1d81715759
dancaps <danny.caperton@gmail.com>
2023-04-10 19:30:34 -0600

    ADD: IntelliJ Files

---
+++ Pangram.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Pangram {
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        Set<Character> alphabet = new HashSet<>();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            alphabet.add(sentence.charAt(i));
+            if (alphabet.size() == 26) {
+                System.out.println("true");
+                return;
+            }
+        }
+        System.out.println("false");
+    }
+}
--- TestingIdeas.java
+++ TestingIdeas.java
@@ -6,6 +6,8 @@ public class TestingIdeas {
         Map<Integer, Integer> myHashMap = new HashMap<>();
 
         myHashMap.put(1, 100);
+        myHashMap.put(100000, 100);
+        //myHashMap.clear();
 
         System.out.println(myHashMap);
     }
